49142715950ce7bb59a9a953861bffc168ac07f5
Eugene Lukin <eugene.lukin@gmail.com>
2025-08-04 16:22:33 +0300

    Fixed some configuration errors; tried to access env variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Geist, Geist_Mono } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import styles from '@styles/Home.module.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,6 +27,9 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
       </div> */}
+      <p>
+        Host: {process.env.NEXT_PUBLIC_HOST}
+      </p>
     </>
   )
 }
