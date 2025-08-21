1a7373472de7fc9c075bac462ad5546e84fbb52b
Eugene Lukin <eugene.lukin@gmail.com>
2025-08-21 04:47:38 +0300

    Created custom toolbar in editor

--- shared/ui/MyQuillEditor/ui/MyQuillEditor.tsx
+++ shared/ui/MyQuillEditor/ui/MyQuillEditor.tsx
@@ -7,11 +7,20 @@ export type EditorProps = {
   onChange: (value: string) => void
 }
 
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],
+  ['blockquote', 'code-block'],
+  [{ header: 1 }, { header: 2 }, { header: 3 }],
+  [{ indent: '-1' }, { indent: '+1' }],
+  [{ color: [] }, { background: [] }],
+]
+
 export const MyQuillEditor: React.FC<EditorProps> = ({ value, onChange }) => {
   return (
     <ReactQuill
       theme="snow"
       value={value}
+      modules={{ toolbar: toolbarOptions }}
       onChange={(value, delta) => {
         console.log('Quill: ' + JSON.stringify(value))
         onChange(value)
